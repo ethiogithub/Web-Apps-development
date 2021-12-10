861b143c4ca02a44f43fa9bfc2fdf770efd037ee
ethiogithub <95913707+ethiogithub@users.noreply.github.com>
2021-12-10 15:27:39 +0300

    Create database_conn.php

---
+++ database_conn.php
@@ -0,0 +1,4 @@
+ <?php
+     $conn = mysqli_connect('localhost','root','r00tme1221') or die ("The connectin is failed");
+           mysqli_select_db($conn,"stu_meal"  ) or die ("The database not connected");
+           ?> 
